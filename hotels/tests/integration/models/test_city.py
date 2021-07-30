c2970817fd7226c46a0b9fd6eba799b56ca5d324
Floor Harmsen <floor.harmsen@gmail.com>
2021-07-30 12:51:07 +0200

    removed the integration tests

--- hotels/tests/integration/models/test_city.py
+++
@@ -1,3 +0,0 @@
-"""
-    This section should check if the city that is created, is saved to the database.
-"""
--- hotels/tests/integration/models/test_hotel.py
+++
@@ -1,3 +0,0 @@
-"""
-    This section should check if the hotel that is created, is saved to the database.
-"""
